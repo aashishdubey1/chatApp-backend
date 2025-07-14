20ec8b3956b2bc8bacbb838d2624dc08bbae07f8
Aashish <aashishdubey.dev@gmail.com>
2025-07-14 21:01:30 +0530

    feat: added user model with password hashing

---
+++ src/models/User.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:[true,"Username is Required"],
+        unique:true,
+        trim:true,
+        minlength:[3,"Username must be at least 3 characters long"]
+    },
+    email:{
+        type:String,
+        required:[true,"Email is required"],
+        trim:true,
+        unique:true,
+        lowercase:true,
+        match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address']
+    },
+    password:{
+        type:String,
+        required:[true,"Password is required"],
+        trim:true,
+        minlength:[8,"Password must be at least 8 characters long"],
+        select:false
+    },
+    created_At:{
+        type:Date,
+        default:Date.now()
+    }
+})
+
+userSchema.pre('save',async (next)=>{
+    if(!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password,12);
+    next();
+})
+
+userSchema.method.comparePassword = async (candidatePassword) => {
+    return await bcrypt.compare(candidatePassword,this.password)
+}
+
+const User = mongoose.model('User',userSchema)
+
+export default User;
